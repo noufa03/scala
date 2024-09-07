24173775b32c306b96ce674fa1b23b0c58f9ea9c
noufa03 <140165529+noufa03@users.noreply.github.com>
2024-09-07 17:10:29 +0530

    Update 22001743_p10.scala

--- 22001743_p10.scala
+++ 22001743_p10.scala
@@ -11,3 +11,50 @@ object occurance{
     }
 
 }
+
+//question 1
+
+class Rational(n:Int,d:Int){
+    require(d!=0)
+    private val gcdValue=gcd(n.abs,d.abs)
+    val numerator:Int=n/gcdValue
+    val denominator:Int=d/gcdValue
+
+    def this(n:Int)=this(n,1)
+
+    if(denominator <0){
+        new Rational(-numerator,-denominator)
+    }
+
+    def neg:Rational=new Rational(-numerator,denominator)
+    //2 question
+     def -(that: Rational): Rational = {
+    new Rational(
+      this.numerator * that.denominator - this.denominator * that.numerator,
+      this.denominator * that.denominator
+    )
+  }
+    override def toString(): String =
+    if(denominator==1) numerator.toString
+    else s"$numerator/$denominator"
+
+    private def gcd(a:Int,b:Int):Int =if (b==0) a else gcd(b,a%b)
+}
+
+
+
+object RationalApp{
+    def main(args:Array[String]):Unit={
+        val x=new Rational(5,8)
+        val y=new Rational(2,7)
+
+        println(s"x:$x")
+        val negX=x.neg
+        println(s"Negated x:$negX")
+        println(sub(x,y))
+    }
+
+    def sub(x:Rational,y:Rational):Rational={
+        x-y
+    }
+}
